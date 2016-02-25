3f57bdbdcfba1c601796387fa9d0caff72d1f691
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-02-25 16:38:18 +0100

    [config.mk] add new package "minimal" to default build

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx
-PACKAGES_LIST_DEFAULT=default backbone
+PACKAGES_LIST_DEFAULT=default backbone minimal
 OPENWRT_SRC=git://github.com/SvenRoederer/openwrt-openwrt.git
 OPENWRT_COMMIT=origin/freifunk-berlin/for-kathleen-0.2_unstable
 MAKE_ARGS=
