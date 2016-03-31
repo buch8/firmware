f5117ddbb87697d0926576185d7f81406e92262d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-03-31 11:15:35 +0200

    switch to kernel 3.18.29 on OpenWrt

--- config.mk
+++ config.mk
@@ -3,5 +3,6 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=default backbone minimal
 OPENWRT_SRC=git://github.com/SvenRoederer/openwrt-openwrt.git
+# 31. March 2016 - switch to kenel 3.18.29
 OPENWRT_COMMIT=origin/freifunk-berlin/for-kathleen-0.2_unstable
 MAKE_ARGS=
